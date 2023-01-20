973f32ec22c3d6dbf080b4563a55a45616e1e431
Christine Yang <yang.christine.95@gmail.com>
2023-01-19 22:54:29 -0500

    Add app to project repo

---
+++ Leo/Leo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Leo
+//
+//  Created by Christine Yang on 1/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Leo/Leo/LeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeoApp.swift
+//  Leo
+//
+//  Created by Christine Yang on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
